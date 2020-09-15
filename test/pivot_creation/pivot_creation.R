5b627d8203c822f9436d1dbc77abb0dc1dd3a841
DiegoBrambilla <diego.brambilla@lnu.se>
2020-09-15 19:35:19 +0200

    Changing optparse-approach with data.table(fread) approach

--- test/pivot_creation/pivot_creation.R
+++ test/pivot_creation/pivot_creation.R
@@ -1,74 +1,21 @@
-#!/usr/bin/env Rscript
+#!/usr/bin/Rscript
 
-args = commandArgs(trailingOnly=TRUE)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(data.table)
 
-if length(args) < 1 {
-  stop("Usage: pivot_creation.r <relab16S.csv> ...")
-}
+BASE_GLOB = '.'
 
-suppressPackageStartupMessages(library(tidyverse))
-suppressPackageStartupMessages(library(optparse))
+samples <- unique( sub( '^([^.]+)\\..*', '\\1',basename( Sys.glob(sprintf("%s/*.csv", BASE_GLOB)))))
+#cat("cat(samples): \n")
+#cat(samples)
 
-OUT="pivot_table.tsv"
-
-# Get arguments
-option_list = list(
-  make_option(
-  c('--outdir'), type='character', default='.',
-  help='Output directory, default is working directory.'
-  ),
-  make_option(
-    c("-f","--filelist"),default=FALSE,
-    help="comma separated list of files (default %default)"
-    ),
-  make_option(
-    c('--tblmark'), type='character', default='.csv',
-    help="Name pattern to identify input file names, default= %default"
-  ),
-  make_option(
-    c('--prefix'), type='character', default='pivot',
-    help='Prefix for output files, default %default.'
-  ),  
-  make_option(
-    c("-v", "--verbose"), action="store_true", default=FALSE, 
-    help="Print progress messages"
-  ),
-)
-
-opt = parse_args(OptionParser(option_list=option_list), positional_arguments=TRUE)
-
-logmsg = function(msg, llevel='INFO') {
-  if ( opt$verbose ) {
-    write(
-      sprintf("%s: %s: %s", llevel, format(Sys.time(), "%Y-%m-%d %H:%M:%S"), msg),
-      stderr()
-    )
+for ( s in samples ) {
+  write(sprintf("*** %s ***", s), stderr())
+  for ( tf in Sys.glob(sprintf("%s/%s.csv", BASE_GLOB, s)) )  {
+    write(sprintf("\t--> %s <--", tf), stderr())
+    tkn <- (sprintf("%s",tf))
+    t <- fread(tkn , sep = ',', stringsAsFactors = FALSE, header = FALSE, data.table = TRUE, fill = TRUE, col.names = c('genes','normalized_values')) %>% head(n = 100)
   }
 }
-
-tables = Sys.glob(sprintf("%s/*.csv", opt$outdir))
-
-
-tbl = tables[grep(opt$tblmark, tables, fixed=T)]
-
-samples = sub(sprintf("%s.*", opt$tblmark), "", basename(tbl))
-
-names(tbl) = samples
-
-# Check if file import and options have been correctly set
-options <- opt$options
-args <- opt$args
-
-myfilelist <- strsplit(options$filelist, "[,:]")
-
-print (myfilelist)
-print(samples)
-print (args)
-
-# Trying to merge files in a single pivot
-mergers = vector('list', length(samples))
-names(mergers) = samples
-for ( s in samples ) {
-  logmsg(sprintf("--> merging %s <--", s))
-  smptbl = add_column (tbl[[s]], sample = tbl[[s]] ) %>%
-    
